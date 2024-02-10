f57c83a8b57f791f91ff860491e633001348a45d
rashidjatoi <rashidjatoi31@gmail.com>
2024-02-10 22:19:48 +0500

    Added themes in Seperate files

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'utils/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ecommerce App',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: EAppTheme.lightTheme,
+      darkTheme: EAppTheme.darkTheme,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
-import 'package:ecommerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ecommerce App',
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: EAppTheme.lightTheme,
-      darkTheme: EAppTheme.darkTheme,
-    );
-  }
+  runApp(const App());
 }
